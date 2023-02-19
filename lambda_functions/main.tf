244b49cdae02253ff0f458a4104ca6d637c66925
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-19 22:24:46 +0530

    Create main.tf

---
+++ lambda_functions/main.tf
@@ -0,0 +1,26 @@
+# lambda_functions/main.tf
+variable "functions" {
+  type = list(object({
+    name            = string
+    handler         = string
+    runtime         = string
+    source_code_hash = string
+  }))
+}
+
+resource "aws_lambda_function" "lambda_functions" {
+  for_each = { for f in var.functions : f.name => f }
+
+  function_name    = each.value.name
+  handler          = each.value.handler
+  runtime          = each.value.runtime
+  source_code_hash = each.value.source_code_hash
+
+  filename         = each.value.name
+
+  source_code_hash = each.value.source_code_hash
+
+  #source_code_size = data.archive_file[each.value.name].output_size
+
+  # Add any other necessary configuration here
+}
