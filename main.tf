7e6fe8404bedc0dd4fb10bf983cc78e38595b302
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-20 00:07:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "archive_file" "function1_archive" {
+data "archive_file" "function1" {
   type        = "zip"
   source_dir  = "function1"
   output_path = "function1.zip"
@@ -25,9 +25,9 @@ module "lambda_functions" {
       name            = "function1"
       handler         = "function1.lambda_handler"
       runtime         = "python3.8"
-      filename       = data.archive_file.function1_archive.output_path
-      source_code_hash = filebase64sha256("${data.archive_file.function1_archive.output_path}")
-      #source_code_hash = data.archive_file.function1_archive.output_base64sha256
+      filename       = data.archive_file.function1.output_path
+      #source_code_hash = filebase64sha256("${data.archive_file.function1.output_path}")
+      source_code_hash = data.archive_file.function1.output_base64sha256
       role = "arn:aws:iam::558940753150:role/lambda-full-acces"
     },
     {
